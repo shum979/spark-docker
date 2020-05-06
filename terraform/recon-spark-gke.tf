134b742f6e02a0223ed44fb9d5d68ad65d521571
shubham gupta <shubham.gupta1@publicissapient.com>
2020-05-06 23:32:41 +0100

    added other necessary files for cluster deployment

---
+++ terraform/recon-spark-gke.tf
@@ -0,0 +1,50 @@
+provider "google" {
+  version = "3.5.0"
+
+  credentials = file("CloudLearnings-4c251a22e3fd.json")
+
+  project = "psyched-canto-269308"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "my-gke-cluster"
+  location = "us-central1"
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  master_auth {
+    username = ""
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  location   = "us-central1"
+  cluster    = google_container_cluster.primary.name
+  node_count = 2
+
+  node_config {
+    preemptible  = true
+    machine_type = "n1-standard-1"
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
